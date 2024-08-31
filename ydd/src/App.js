01ce27a0b9c7a0229659093480a30eef00d0a6b5
lawrence lu <1992462850@qq.com>
2024-09-01 02:34:27 +0800

    11

--- ydd/src/App.js
+++ ydd/src/App.js
@@ -10,7 +10,7 @@ function App() {
             <div className='leftbox2'></div>
             <div className='leftbox3'>Ydd Dashboard</div>
           </div>
-          <div className='title_right_box'>最新更新时间：2024/8/30</div>
+          <div className='title_right_box'>最新更新时间：2024/8/31</div>
         </div>
       </div>
       <div className='dashboard'>
