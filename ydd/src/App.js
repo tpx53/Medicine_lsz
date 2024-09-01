9736332bc9694da297043264abd1d350de231da7
Lawrence lu <1992462850@qq.com>
2024-09-01 18:12:42 +0800

    主页构版

--- ydd/src/App.js
+++ ydd/src/App.js
@@ -14,7 +14,19 @@ function App() {
         </div>
       </div>
       <div className='dashboard'>
-
+        <div className='dashboard_top'>
+          <div className='basic_infor_title'>
+            <div className='title_name'>基本信息</div>
+          </div>
+          <div className='basic_infor'>
+            <div className='basic_infor_box'>
+              <div className='basic_infor1'>1</div>
+              <div className='basic_infor2'>2</div>
+            </div>
+          </div>
+        </div>
+        <div className='dashboard_floor'></div>
+      
       </div>
     </div>
   );
